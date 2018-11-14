603d9f61105980485c85c39a7dffedd571aee5ba
BullsI808 <JBasuil8697@myhta.org>
2018-11-14 12:32:10 -1000

    finished

--- check-ascending-order/checkAscOrder.js
+++ check-ascending-order/checkAscOrder.js
@@ -1,3 +1,6 @@
 module.exports = function checkAscOrder(numArray) {
   // write your code in here
+  var i = 0;
+  var before = i-1;
+  var after = i+1;
 }
--- cookie-problem/cookieProblem.js
+++ cookie-problem/cookieProblem.js
@@ -1,3 +1,23 @@
 module.exports = function cookieProblem(array) {
-  // write your code in here
+  // find the highest number in array because this represents the highest amt of cookies a person has
+  let highestCookies = 0;
+  let currentTotal = 0;
+
+  array.forEach(cookies => {
+    highestCookies = math.max(highestCookies, cookies);
+  });
+
+  currentTotal += cookies;
+  return highestCookies * array.length - currentTotal;
+
+  // return array,map(function(subarray){
+  //   return math.max.apply(null, subarray);
+  // });
+
+
+  // find the sum of cookies everyone currently has
+
+  // find the sum of cookies that everyone SHOULD have if they all had the same amt of cookies
+
+  // subtract the sum of cookies everyone currently has from the sum of cookies everyone should have
 }
