281ef2336e21f74992bbeb7597f1c28e01302b33
BullsI808 <JBasuil8697@myhta.org>
2018-12-05 12:24:52 -1000

    finished

--- needle-in-haystack/needleInHaystack.js
+++ needle-in-haystack/needleInHaystack.js
@@ -1,3 +1,23 @@
 module.exports = function needleInHaystack(haystack) {
   // write code in here
+
+  //failed code
+
+  // var needleLocations = [];
+  // var find = 'needle';
+  // var findNeedle = array.indexOf(find);
+  // while (find != -1){
+  //   needleLocations.push(findNeedle);
+  //   findNeedle = array.indexOf(needle, findNeedle + 1);
+  // }
+  // return needleLocations;
+
+
+    if(haystack.includes('needle')){
+      return haystack.indexOf('needle');
+    } else{
+      return false;
+    }
+
+
 }
